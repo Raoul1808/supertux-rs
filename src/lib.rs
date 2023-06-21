98d56f340e487658be226da288322fe981d2f8b9
Raoul1808 <raoulthegeek@gmail.com>
2023-06-21 15:49:01 +0200

    Insert commit name

--- src/lib.rs
+++ src/lib.rs
@@ -33,11 +33,11 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex { position: [-0.25, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [0.25, 0.5, 0.0], color: [1.0, 1.0, 0.0] },
-    Vertex { position: [0.5, 0.0, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.25, -0.5, 0.0], color: [0.0, 1.0, 1.0] },
-    Vertex { position: [-0.25, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
-    Vertex { position: [-0.5, 0.0, 0.0], color: [1.0, 0.0, 1.0] },
+    Vertex { position: [-0.5, 0.0, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.25, -0.5, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.25, -0.5, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.5, 0.0, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.25, 0.5, 0.0], color: [0.5, 0.0, 0.5] },
 ];
 
 const INDICES: &[u16] = &[
